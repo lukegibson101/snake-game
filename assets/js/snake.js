367cff073e675bb8464b4380564eb592d8693cd2
Luke Gibson <lukegibson101@gmail.com>
2021-09-26 21:24:09 +0000

    enable snake to turn direction with key press

--- assets/js/snake.js
+++ assets/js/snake.js
@@ -1,26 +1,6 @@
 // wait for the DOM to finish loading before running the game
 document.addEventListener("DOMContentLoaded", function () {
-    // get canvas element
-    let gameBoard = document.getElementById("snakeBoard");
-
-    // set canvas to 2d drawing context
-    let gameBoardCtx = gameBoard.getContext("2d");
-
-
-    /**
-     * drawCanvas will be called often to "reset" the game board to allow the updated movements to then be drawn so there is no overlap of the previous frame
-     */
-    function drawCanvas() {
-        // background color of canvas
-        gameBoardCtx.fillStyle = '#c0c0c0';
-        // canvas border color
-        gameBoardCtx.strokeStyle = '#000'
-        // Draw a filled rectangle to cover the canvas
-        gameBoardCtx.fillRect(0, 0, gameBoard.width, gameBoard.height);
-        // draw the canvas border
-        gameBoardCtx.strokeRect(0, 0, gameBoard.width, gameBoard.height);
-    }
-
+    
     let snake = [{
             x: 100,
             y: 500
@@ -43,13 +23,55 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     ]
 
-
-    // horizontal movement  
+    // change to true if changiung direction
+    let changingSnakeDirection = false;
+    // initial horizontal movement  
     let dx = 20;
-    // vertical movement
+    // initial vertical movement
     let dy = 0;
     // inital speed
     let speed = 300;
+
+    // get canvas element
+    let gameBoard = document.getElementById("snakeBoard");
+
+    // set canvas to 2d drawing context
+    let gameBoardCtx = gameBoard.getContext("2d");
+
+
+    /**
+     * drawCanvas will be called often to "reset" the game board to allow the updated movements to then be drawn so there is no overlap of the previous frame
+     */
+    function drawCanvas() {
+        // background color of canvas
+        gameBoardCtx.fillStyle = '#c0c0c0';
+        // canvas border color
+        gameBoardCtx.strokeStyle = '#000'
+        // Draw a filled rectangle to cover the canvas
+        gameBoardCtx.fillRect(0, 0, gameBoard.width, gameBoard.height);
+        // draw the canvas border
+        gameBoardCtx.strokeRect(0, 0, gameBoard.width, gameBoard.height);
+    }
+
+    //Begin game
+    playGame();
+
+    // listen for keypress to change direction
+    document.addEventListener("keydown", changeSnakeDirection);
+
+    function playGame() {
+
+        changingSnakeDirection = false;
+        setTimeout(function onTick() {
+            drawCanvas();
+            moveSnake();
+            drawSnake();
+            // call again to refresh
+            playGame();
+        }, speed)
+    }
+
+
 
     /**
      * To draw the snake on the canvas
@@ -69,10 +91,62 @@ document.addEventListener("DOMContentLoaded", function () {
         gameBoardCtx.strokeRect(snakeSection.x, snakeSection.y, 20, 20);
     }
 
+
+     /**
+     * This funtion detects if the snake has collided with anything and then calls game over
+     */
+     function collisionDetection() {
+
+     }
+    
+
+    /** Lets get the snake changing direction! */
+    function changeSnakeDirection(event) {
+        // define key presses for directions
+        let arrowLeft = 37;
+        let arrowRight = 39;
+        let arrowUp = 38;
+        let arrowDown = 40;
+        
+
+        // prevent snake from reversing
+        if (changingSnakeDirection) return;
+        changingSnakeDirection = true;
+
+        // get the code for the pressed key
+        let keyPressed = event.keyCode;
+
+        //define what action to take depending on snake direction
+        let leftDir = dx === -20;
+        let rightDir = dx === 20;
+        let upDir = dy === -20;
+        let downDir = dy === 20;
+
+        if (keyPressed === arrowLeft && !rightDir) {
+            dx = -20;
+            dy = 0;
+        }
+
+        if (keyPressed === arrowRight && !leftDir) {
+            dx = 20;
+            dy = 0;
+        }
+
+        if (keyPressed === arrowUp && !downDir) {
+            dx = 0;
+            dy = -20;
+        }
+
+        if (keyPressed === arrowDown && !upDir) {
+            dx = 0;
+            dy = 20;
+        }
+    }
+
     /**
      * Move the snake. Update snake array my adding to front of snake array in the direction of travel and removing the last item in the array
      */
-    function moveSnake() {
+     function moveSnake() {
         let front = {
             x: snake[0].x + dx,
             y: snake[0].y + dy
@@ -81,29 +155,8 @@ document.addEventListener("DOMContentLoaded", function () {
         snake.pop();
     }
 
-    /** Lets get the snake changing direction! */
-    function changeSnakeDirection() {
+   
 
-    }
-
-    /**
-     * This funtion detects if the snake has collided with anything and then calls game over
-     */
-    function collisionDetection() {
-        
-    }
-
-    function playGame() {
-        setTimeout(function onTick() {
-            drawCanvas();
-            moveSnake();
-            drawSnake();
-            // call again to refresh
-            playGame();
-        }, speed)
-    }
-
-    // lets start the game
-    playGame();
+     
 
 }) //end DOM loaded function

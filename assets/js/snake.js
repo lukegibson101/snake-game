2153ca30dde5a1f417a6ce8217aab731c3c0a437
Luke Gibson <lukegibson101@gmail.com>
2021-09-26 17:35:35 +0000

    set drawCanvas to creat game area

--- assets/js/snake.js
+++ assets/js/snake.js
@@ -1,8 +1,21 @@
 // get canvas element
-let gameboard = document.getElementById("snakeBoard");
+let gameBoard = document.getElementById("snakeBoard");
 
 // set canvas to 2d drawing context
-let gameboard_ctx = gameCanvas.getContext("2d");
+let gameBoardCtx = gameBoard.getContext("2d");
 
+drawCanvas();
 
-
+/**
+ * drawCanvas will be called often to "reset" the game board to allow the updated movements to then be drawn so there is no overlap of the previous frame
+ */
+function drawCanvas () {
+    // background color of canvas
+    gameBoardCtx.fillStyle = '#c0c0c0';
+    // canvas border color
+    gameBoardCtx.strokeStyle = '#000'
+    // Draw a filled rectangle to cover the canvas
+    gameBoardCtx.fillRect(0, 0, gameBoard.width, gameBoard.height);
+    // draw the canvas border
+    gameBoardCtx.strokeRect(0, 0, gameBoard.width, gameBoard.height);
+}

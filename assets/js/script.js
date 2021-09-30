83ef5168ba5a507b96c21fad787e6d991f800ac8
Luke Gibson <lukegibson101@gmail.com>
2021-09-30 19:33:47 +0000

    set script to close model on closebutton click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,35 @@
 let modalBtn = document.getElementsByClassName('nav-btn');
+for (let i = 0; i < modalBtn.length; i++) {
+    modalBtn[i].addEventListener('click', showModal);
 
-    for (let i = 0; i < modalBtn.length; i++) {
-        modalBtn[i].addEventListener('click', showModal);
-       
-    }
+}
 
-    function showModal () {
-        if (this.getAttribute("id") === "settings-btn") {
-            document.getElementById('settings-modal').style.display = 'block';
-        }
-        if (this.getAttribute("id") === "how-btn") {
-          document.getElementById('how-modal').style.display = 'block';
-        }
+function showModal() {
+    if (this.getAttribute("id") === "settings-btn") {
+        document.getElementById('settings-modal').style.display = 'block';
+    }
+    if (this.getAttribute("id") === "how-btn") {
+        document.getElementById('how-modal').style.display = 'block';
+    }
+}
 
-    }
-    
-    let modalClose = document.getElementsByClassName('modal');
-    for (let i = 0; i < modalClose.length; i++) {
-        modalClose[i].addEventListener('click', closeModal);
-        
-    }
+let modalClose = document.getElementsByClassName('modal');
+for (let i = 0; i < modalClose.length; i++) {
+    modalClose[i].addEventListener('click', closeModal);
+}
+let clickClose = document.getElementsByClassName('close');
+for (let i = 0; i < clickClose.length; i++) {
+   clickClose[i].addEventListener('click', closeModal);
+}
 
-    function closeModal () {
-        if (this.getAttribute("id") === "settings-modal") {
-            document.getElementById('settings-modal').style.display = 'none';
-        }
-        if (this.getAttribute("id") === "how-modal") {
-          document.getElementById('how-modal').style.display = 'none';
-        }
-        
+function closeModal() {
+    if (this.getAttribute("id") === "settings-modal") {
+        document.getElementById('settings-modal').style.display = 'none';
     }
+    if (this.getAttribute("id") === "how-modal") {
+        document.getElementById('how-modal').style.display = 'none';
+    }
+}
+
+
+

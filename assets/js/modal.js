2f4451f649d3d98b47679a7da462a5f0357676f0
Luke Gibson <lukegibson101@gmail.com>
2021-10-04 00:24:57 +0000

    add comments

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -4,6 +4,9 @@ for (let i = 0; i < modalBtn.length; i++) {
 
 }
 
+/**
+ * Displays modal on screen when button clicked
+ */
 function showModal() {
     if (this.getAttribute("id") === "settings-btn") {
         document.getElementById('settings-modal').style.display = 'block';
@@ -13,15 +16,18 @@ function showModal() {
     }
 }
 
-
 document.getElementById('how-close').onclick = function() {
     document.getElementById('how-modal').style.display = "none";
-  }
+  };
 
 document.getElementById('settings-close').onclick = function() {
     document.getElementById('settings-modal').style.display = "none";
-  }
+  };
 
+  /**
+   * Clears modal
+   * @param {click} event 
+   */
 window.onclick = function(event) {
     if (event.target == document.getElementById('settings-modal')) {
         document.getElementById('settings-modal').style.display = "none";
@@ -29,7 +35,4 @@ window.onclick = function(event) {
     if (event.target == document.getElementById('how-modal')) {
         document.getElementById('how-modal').style.display = "none";
     }
-  }
-
-
-  
+  };
